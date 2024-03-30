e3e08a4dd067dec6596c59bd24795b26dcff31aa
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-03-30 22:20:29 +0100

    Add base.html which extends to product_list/detail_view, add not complete nav bar

--- product/views.py
+++ product/views.py
@@ -1,9 +1,7 @@
 #----------------------------------------------------------------------------------------------PRODUCT/VIEWS.PY
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
-import logging
 
-logger = logging.getLogger(__name__)
 
 def product_list_view(request):
     """ A view to show all products """
@@ -17,10 +15,8 @@ def product_list_view(request):
 def product_detail_view(request, product_id):
     """ A view to show product details """
     product = get_object_or_404(Product, pk=product_id)
-    context = {
-        'product': product,
-    }
-    return render(request, 'product/product_detail_view.html', context)
+
+    return render(request, 'product/product_detail_view.html', {'product': product})
 
 def product_add_view(request):
     """ A view to add a new product """
